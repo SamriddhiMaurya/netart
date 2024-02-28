ff0faa26cdfa3dab8eb42e4f122d97258ec9862d
SamriddhiMaurya <samriddimaurya9005@gmail.com>
2024-02-28 22:24:13 +0530

    okkk

--- src/components/Award.js
+++ src/components/Award.js
@@ -63,7 +63,7 @@ const AwardComponent = () => {
                 alt="Award"
                 width="300"
                 height="500"
-                // style={{ aspectRatio: "600 / 200", objectFit: "cover" }}
+                 style={{ aspectRatio: "600 / 200", objectFit: "cover" }}
               />
             </div>
           </div>
@@ -83,13 +83,14 @@ const AwardComponent = () => {
             height="200"
             style={{ aspectRatio: "600 / 200", objectFit: "cover" }}
           />
-          <p className="text-xs text-center">
+          <p className="prod text-center">
             Valves - Pumps - Pipes - IoT Drives &amp; Controllers - Wires &amp;
             Cables - Solar Systems - Motors
           </p>
+          <hr  color='red'/>
         </section>
         <section className="mb-4">
-          <h3 className="text-md font-semibold mb-2">
+          <h3 className="text-md font-semibold mb-2 ">
             C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS
           </h3>
           <p className="text-xs mb-4">
@@ -102,59 +103,21 @@ const AwardComponent = () => {
           <hr />
         </section>
         <footer className="flex justify-between items-center p-4">
-          <div className="flex items-center space-x-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="text-red-600 mb-2"
-            >
-              <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
-            </svg>
-            <span className="text-xs">Toll free 1800 200 1234</span>
-          </div>
-          <div className="flex items-center space-x-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="text-blue-600 mb-2"
-            >
-              <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-            </svg>
-            <span className="text-xs">www.facebook.com/cripumps</span>
-          </div>
-          <div className="flex items-center space-x-4">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="text-green-600 mb-2"
-            >
-              <circle cx="12" cy="12" r="10"></circle>
-              <line x1="2" x2="22" y1="12" y2="12"></line>
-              <path d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"></path>
-            </svg>
-            <span className="text-xs">www.crigroups.com</span>
-          </div>
+          <img
+            className="tele"
+            src="./Netart-Test/asserts/tele.png"
+            alt="Toll free 1800 200 1234"
+          />
+          <img
+            className="fb"
+            src="./Netart-Test/asserts/fb.png"
+            alt="www.facebook.com/cripumps"
+          />
+          <img
+            src="./Netart-Test/asserts/weblogo.png"
+            alt="www.crigroups.com"
+            className="web"
+          />
         </footer>
       </main>
     </div>
